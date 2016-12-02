d53a8d3eeaa7beee94a04481e0733b1f1d064819
shane <shane.kennedy@stonybrook.edu>
2016-12-02 16:33:18 -0500

    PageDao NOW has getPosts

--- src/java/bean/DAO/PageDao.java
+++ src/java/bean/DAO/PageDao.java
@@ -23,13 +23,40 @@ import javax.naming.NamingException;
 
 public class PageDao {
     
-    public static List GetPosts(){
-        List<Post> Posts = new ArrayList<Post>();
+    public static List GetPosts(String pageid) throws SQLException {
+        List<Post> posts = new ArrayList();
+        Connection conn = null;
+        try {
+            InitialContext ctx = new InitialContext();
+            DataSource ds = (DataSource)ctx.lookup("Bazaar_Application_Connection");
+            conn = (Connection) ds.getConnection();
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM Post WHERE"
+                    + "pageid = ?");
+            
+            ps.setString(1, pageid);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                Post post = new Post(
+                        rs.getString("postid"),
+                        rs.getString("authorid"),
+                        rs.getString("content"),
+                        rs.getString("postedDate"),
+                        rs.getString("pageId"),
+                        CommentDao.getComments(rs.getString("postid")),
+                        LikeDao.getPostLikes(rs.getString("postid"))
+                );
+                posts.add(post);
+            }
+            
+        }
+        catch(NamingException | SQLException e){
+            System.out.println(e);
+        }
+        conn.close();
         
-        
-        
-        
-        return Posts;
+        return posts;
     }
     
     public static String getPageIdFromUserId(String userid) throws SQLException{
